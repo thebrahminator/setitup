06b08c61c8a093372a867008e8ac29479d73df91
Mohamed Saleem <mohsal.saleem@gmail.com>
2016-03-28 22:34:31 +0530

    Added code to install Rust

--- script.sh
+++ script.sh
@@ -10,7 +10,19 @@ prog_lang () {
 	done
 	read -p "Enter options seperated by comma..(eg. 11,12) (0 to go to main menu).. " OPTION
 	case "$OPTION" in
-		"0") main_menu
+		"0") main_menu;;
+		"11") final_list+=" 11.Java"
+		prog_lang
+		;;
+		"12") final_list+=" 12.Ruby"
+		prog_lang
+		;;
+		"13") final_list+=" 13.Php"
+		prog_lang
+		;;
+		"14") final_list+=" 14.Rust"
+		prog_lang
+		;;
 	esac
 }		
 
@@ -22,7 +34,25 @@ frameworks () {
 	done	
 	read -p "Enter options seperated by comma..(eg. 21,22) (0 to go to main menu).." OPTION
 	case "$OPTION" in
-		"0") main_menu
+		"0") main_menu;;
+		"21") final_list+=" 21.Rails"
+		frameworks
+		;;
+		"22") final_list+=" 22.Laravel"
+		frameworks
+		;;
+		"23") final_list+=" 23.Django"
+		frameworks
+		;;
+		"24") final_list+=" 24.Node.js"
+		frameworks
+		;;
+		"25") final_list+=" 25.Meteor"
+		frameworks
+		;;
+		"26") final_list+=" 26.Phoenix"
+		frameworks
+		;;
 	esac
 }
 
@@ -33,7 +63,25 @@ text_editors () {
 	done
 	read -p "Enter options seperated by comma..(eg. 31,32) (0 to go to main menu).." OPTION
 	case "$OPTION" in
-		"0") main_menu
+		"0") main_menu;;
+		"31") final_list+=" 31.Vim"
+		text_editors
+		;;
+		"32") final_list+=" 32.GNU/Emacs"
+		text_editors
+		;;
+		"33") final_list+=" 33.Sublime"
+		text_editors
+		;;
+		"34") final_list+=" 34.Atom"
+		text_editors
+		;;
+		"35") final_list+=" 35.Komodo"
+		text_editors
+		;;
+		"36") final_list+=" 36.Brackets"
+		text_editors
+		;;
 	esac	
 }
 
@@ -44,7 +92,16 @@ version_control () {
 	done
 	read -p "Enter options seperated by comma..(eg. 41,42) (0 to go to main menu).." OPTION
 	case "$OPTION" in
-		"0") main_menu
+		"0") main_menu;;
+		"41") final_list+=" 41.Git"
+		version_control
+		;;
+		"42") final_list+=" 42.Mercurial"
+		version_control
+		;;
+		"43") final_list+=" 43.SVN"
+		version_control
+		;;
 	esac
 }
 
@@ -55,7 +112,22 @@ terminals () {
 	done
 	read -p "Enter options seperated by comma..(eg. 51,52) (0 to go to main menu).." OPTION
 	case "$OPTION" in
-		"0") main_menu
+		"0") main_menu;;
+		"51") final_list+=" 51.Terminator"
+		terminals
+		;;
+		"52") final_list+=" 52.Tilda"
+		terminals
+		;;
+		"53") final_list+=" 53.Guake"
+		terminals
+		;;
+		"54") final_list+=" 54.Yakuake"
+		terminals
+		;;
+		"55") final_list+=" 55.Konsole"
+		terminals
+		;;
 	esac
 }
 
@@ -66,18 +138,39 @@ databases () {
 	done
 	read -p "Enter options seperated by comma..(eg. 61,62) (0 to go to main menu).." OPTION
 	case "$OPTION" in
-		"0") main_menu
+		"0") main_menu;;
+		"61") final_list+=" 61.MySql"
+		databases
+		;;
+		"62") final_list+=" 62.Postgresql"
+		databases
+		;;
+		"63") final_list+=" 63.Mongodb"
+		databases
+		;;
+		"64") final_list+=" 64.Redis"
+		databases
+		;;
 	esac
 }
 
 shell () {
 	clear 
-	for i in "71.Bash\n72.Zsh\n73.Tcsh\n75.Kcsh"; do
+	for i in "71.Zsh\n72.Tcsh\n73.Kcsh"; do
 		echo -e $i
 	done
 	read -p "Enter options seperated by comma..(eg. 71,72) (0 to go to main menu).." OPTION
 	case "$OPTION" in
-		"0") main_menu
+		"0") main_menu;;
+		"71") final_list+=" 71.Bash"
+		shell
+		;;
+		"72") final_list+=" 72.Tcsh"
+		shell
+		;;
+		"73") final_list+=" 73.Kcsh"
+		shell
+		;;
 	esac
 }
 
@@ -88,7 +181,16 @@ other_tools () {
 	done
 	read -p "Enter options seperated by comma..(eg. 81,82) (0 to go to main menu).." OPTION
 	case "$OPTION" in
-		"0") main_menu
+		"0") main_menu;;
+		"81") final_list+=" 81.Gimp"
+		other_tools
+		;;
+		"82") final_list+=" 82.ImageMagick"
+		other_tools
+		;;
+		"83") final_list+=" 83.ffmpeg"
+		other_tools
+		;;
 	esac
 }
 
@@ -100,6 +202,16 @@ browsers () {
 	read -p "Enter options seperated by comma..(eg. 91,92) (0 to go to main menu).." OPTION
 	case "$OPTION" in
 		"0") main_menu
+		;;
+		"91") final_list+=" 91.Firefox-dev"
+		browsers
+		;;
+		"92") final_list+=" 92.GoogleChrome"
+		browsers
+		;;
+		"93") final_list+=" Chromium"
+		browsers
+		;;
 	esac
 }
 
@@ -132,6 +244,21 @@ oh_my_zsh () {
 	esac
 }
 
+start_install () {
+	clear
+	echo "List of softwares to be installed: "
+	for i in $final_list; do
+		echo -e $i
+		if [ "$i" == "14.Rust" ]; then
+  			sudo apt-get update
+			sudo apt-get dist-upgrade -y
+			sudo apt-get install curl
+			curl -sSf https://static.rust-lang.org/rustup.sh | sh
+		fi
+		main_menu
+	done
+}
+
 main_menu () {
 	echo "Current List: "
 	for i in $final_list; do
@@ -149,7 +276,7 @@ main_menu () {
 	echo "9. Browsers"
 	echo "10. Container"	
 	echo "11. OhMyZsh"
-	read -p "Enter your choice[1-10]... " CHOICE
+	read -p "Enter your choice[1-10]..., 00 to start installing.. " CHOICE
 	case "$CHOICE" in
 		"1") prog_lang
 		;;
@@ -172,6 +299,8 @@ main_menu () {
 		"10") containers
 		;;
 		"11") oh_my_zsh
+		;;
+		"00") start_install
 		;;
 		*) echo "Wrong option.. Enter a valid one"
 		;;
